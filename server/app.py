c02fbd4b84cc205a7104266fdcf5ed8dcf990b06
Cristal Davila <cristal.davila@gmail.com>
2023-10-12 17:33:39 -0600

    completes without error checking

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
 import os
+from flask import func 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get(
@@ -21,6 +22,143 @@ migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
 
+@app.route('/')
+def home():
+    return ''
+
+
+#GET /actors
+
+@app.get('/actors')
+def get_actors():
+    actors = Actor.query.all():
+    data = [actor.to_dict(only=('name', 'age')) for actor in actors]
+
+    return make_response(
+        jsonify(data),
+        200
+    )
+#GET /actors/int:id
+
+@app.get('/actors/<int:id>')
+def get_actor_by_id(id):
+    actor = Actor.query.filter(Actor.id == id).first()
+
+    if not actor: 
+        return make_response(
+            jsonify({"error": "actor not found"})
+            404
+        )
+#POST /actors
+
+@app.post('/actors')
+def post_actor():
+    data = request.get_json()
+
+    try:
+        new_actor = Actor(
+            name=data.get("name")
+            age=data.get("age")
+        )
+        db.session.add(new_actor)
+        db.session.commit()
+
+        return make_response(
+            jsonify(new_actor.to_dict(only=('name', 'age', 'id'))),
+            201
+        )
+    except ValueError:
+        return make_response(
+            jsonify({"error": ["validation errors"]})
+            406
+        )
+
+#PATCH /actors/:id
+
+@app.patch('/actors/<int:id>')
+def patch_actor_by_id(id):
+    actor = Actor.query.filter(Actor.id == id).first()
+    data = request.get_json()
+
+    if not actor:
+        return make_response(
+            jsonify({"error": "actor not found."}),
+            404
+        )
+    try: 
+        for field in data:
+            setattr(actor, field, data[field])
+            db.session.add(actor)
+            db.session.commit()
+
+        return make_response(
+            jsonify(actor.to_dict(only=('name', 'age', 'id')))
+            202
+        )
+    except ValueError as e:
+        print(e.__str__())
+        return make_response(
+            jsonify({"error": ["validation errors."]})
+            406
+        )
+
+#DELETE /actors/int:id
+
+@app.delete('/actors/<int:id>')
+def delete_actor(id):
+    actor = Actor.query.filter(Actor.id == id).first()
+
+    if not actor: 
+        return make_response(
+            jsonify({"error": "Actor Not Found"}),
+            404
+        )
+    db.sesssion.delete(actor)
+    db.session.commit()
+
+    return make_response(jsonify({}), 204
+)
+
+#GET /movies
+
+@app.get('/movies')
+def get_movies():
+    movies = Movie.query.all()
+    data = [movie.to_dict(only=('image', 'title', 'genre', 'rating', 'description')) for movie in movies]
+
+    return make_response(
+        jsonify(data),
+        200
+    )
+
+
+#POST /movies
+
+@app.post('/movies')
+def post_movie():
+    data = request.get_json()
+
+    try:
+        new_movie = Movie(
+            image=data.get("image")
+            title=data.get("title")
+            genre=data.get("genre")
+            rating=data.get("rating")
+            description=data.get("description")
+
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+
+        return make_response(
+            jsonify(new_movie.to_dict(only="title")),
+            201
+        )
+    except ValueError:
+        return make_response(
+            jsonify({"error": ["validation errors"]}),
+            406
+        )
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -19,8 +19,72 @@ db = SQLAlchemy(metadata=metadata)
 class Movie(db.Model, SerializerMixin):
     __tablename__='movie_table'
 
-class Actor(db.Model, SerializerMixin):
-    __tablename__='actor_table'
+    id = db.Column(db.Integer, primary_key = True)
+    image = db.Column(db.String)
+    title = db.Column(db.String)
+    genre = db.Column(db.String)
+    rating = db.Column(db.Integer, Nullable = False)
+    description = db.Column(db.String)
+
+credit_movie_relationship = db.relationship('Credit', back_populates= 'movie_relationship')
+
+
+@validates('rating')
+def validates_rating(self, key, rating):
+    if  1 <= rating >= 10:
+        return rating
+    else:
+        raise ValueError("Not a Valid Rating, must be between 1 and 10.")
+
+@validates('genre')
+def validates_genre(self, key, genre): 
+    valid_genre = ['Action', 'Comedy', 'Drama', 'Horror', 'Romance', 'Thriller', 'Science Fiction', 'Fantasy', 'Mystery', 'Adventure', 'Crime', 'Family', 'Animation', 'Documentary', 'War']
+    if genre in valid_genre:
+        return valid_genre
+    else:
+        raise ValueError("Not a valid genre.")
 
 class Credit(db.Model, SerializerMixin):
     __tablename__='credit_table'
+
+    id = db.Column(db.Integer, primary_key = True)
+    actor_id = db.Column(db.Integer, Nullable = False)
+    movie_id = db.Column(db.Integer, Nullable = False)
+    role = db.Column(db.String, Nullable = False)
+
+    actor_id = db.Column(db.Integer, db.ForeignKey('actor_table.id'))
+    actor_relationship = db.relationship('Axtor', back_populates='credit_actor_relationship')
+
+
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie_table.id'))
+    movie_relationship = db.relationship('Movie', back_populates='credit_movie_relationship')
+
+    @validates('roles')
+    def validates_roles(self, key, roles): 
+        valid_roles = ['Performer', 'Director', 'Producer', 'Playwright', 'Lighting Design', 'Sound Design', 'Set Dersign']
+        if roles in valid_roles:
+            return valid_roles
+        else:
+            raise ValueError("Not a valid role.")
+
+
+class Actor(db.Model, SerializerMixin):
+    __tablename__='actor_table'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, Nullable = False)
+    age = db.Column(db.String, Nullable = False)
+
+    credit_actor_relationship = db.relationship('Credit', back_populates = 'actor_relationship')
+
+    @validates('age')
+    def validate_age(self, key, age):
+        if not age >= 10:
+            raise ValueError("Invalid age, must be at least 10 years old.")
+        return age
+
+
+
+
+       
+    
